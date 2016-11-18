d3d8c52e473f1fc3d36e598340cd79e0efa776fe
Elazar Gershuni <elazarg@gmail.com>
2016-11-18 10:17:30 +0200

    add whitelist.py

---
+++ examples/tmp.py
@@ -0,0 +1,8 @@
+def func1():
+    pass
+
+def func2():
+    pass
+
+dispatcher = {0:func1, 1:func2}
+dispatcher[0]()
---
+++ whitelist.py
@@ -0,0 +1,33 @@
+import re
+
+
+class Flags:
+    include_strings = True
+    ignore_underscored_methods = True
+    ignore_underscored = True
+    track_classes = False
+    track_variables = True
+
+
+class Frameworks:    
+    ast = True
+    pytest = True
+
+
+def get_matcher(method_prefix = r'\.'):
+    def methods(*items):
+        return [method_prefix + x for x in items]
+
+    prefixes = methods('__.+')
+    if Frameworks.ast:
+        prefixes += methods('generic_visit', 'visit_.+')
+    if Frameworks.pytest:
+        prefixes += ['test_.+', 'call', 'pytest_.*', ] 
+        prefixes += methods('test_.+', 'runtest', 'run_test', 'set_up', 'setup', 'teardown', 'cases')
+    if Flags.ignore_underscored_methods:
+        prefixes += methods('_.+')
+    if Flags.ignore_underscored:
+        prefixes.append('_.+')
+    
+    return re.compile('|'.join('({})'.format(p) for p in prefixes)).fullmatch
+    
