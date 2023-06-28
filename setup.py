aa6b92580ebdd6ec91c4d5026d1cbf58c371823d
Elazar Gershuni <elazarg@gmail.com>
2023-06-28 22:53:35 +0300

    update package info

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.4',
+    version='0.1.7',
 
     description='Find unused functions in Python projects',
     long_description=long_description,
@@ -31,7 +31,7 @@ setup(
     author_email='elazarg@gmail.com',
 
     # Choose your license
-    license='GNU',
+    license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -46,7 +46,7 @@ setup(
         'Topic :: Software Development :: Quality Assurance',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
@@ -54,6 +54,9 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 
     # What does your project relate to?
